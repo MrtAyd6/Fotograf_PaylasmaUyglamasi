9dc4da09f298b9056ab4ed2f66cbc906735a414e
MrtAyd6 <mrmurat5757@gmail.com>
2025-03-23 18:44:18 +0300

    kullanici kayit,giris ve cikis fonksiyonlari yazildi

--- app/src/main/java/com/example/fotografpaylasma/KullaniciFragment.kt
+++ app/src/main/java/com/example/fotografpaylasma/KullaniciFragment.kt
@@ -5,19 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.navigation.Navigator
 import com.example.fotografpaylasma.databinding.FragmentKullaniciBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class KullaniciFragment : Fragment() {
 
     private var _binding : FragmentKullaniciBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
@@ -33,15 +38,45 @@ class KullaniciFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.kayitButton.setOnClickListener{ kayitOl(it)}
         binding.girisButton.setOnClickListener { girisYap(it) }
+
+        val guncelKullanici = auth.currentUser
+        if(guncelKullanici != null){
+            //kullanıcı daha önce giriş yapmış
+            val action = KullaniciFragmentDirections.actionKullaniciFragmentToFeedFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
     }
 
     fun kayitOl(view:View){
-        val action = KullaniciFragmentDirections.actionKullaniciFragmentToFeedFragment()
-        Navigation.findNavController(view).navigate(action)
+
+
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordtext.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    //kullanıcı oluşturuldu
+                    val action = KullaniciFragmentDirections.actionKullaniciFragmentToFeedFragment()
+                    Navigation.findNavController(view).navigate(action)
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(requireContext(),exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     fun girisYap(view:View){
-
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordtext.text.toString()
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+                val action = KullaniciFragmentDirections.actionKullaniciFragmentToFeedFragment()
+                Navigation.findNavController(view).navigate(action)
+            }.addOnFailureListener { exceprion ->
+                Toast.makeText(requireContext(),exceprion.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 
