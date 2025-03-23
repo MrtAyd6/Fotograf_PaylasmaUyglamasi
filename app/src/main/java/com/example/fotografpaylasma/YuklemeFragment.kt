d6076ef9017d19c61be5ce83521501fa82f79408
MrtAyd6 <mrmurat5757@gmail.com>
2025-03-23 19:30:52 +0300

    görsel secim izinleri yapıldı

--- app/src/main/java/com/example/fotografpaylasma/YuklemeFragment.kt
+++ app/src/main/java/com/example/fotografpaylasma/YuklemeFragment.kt
@@ -1,21 +1,43 @@
 package com.example.fotografpaylasma
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.media.Image
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 import com.example.fotografpaylasma.databinding.FragmentYuklemeBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class YuklemeFragment : Fragment() {
     private var _binding : FragmentYuklemeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var secilenGorsel : Uri? = null
+    var secilenBitmap : Bitmap? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        registerLaunchers()
     }
 
     override fun onCreateView(
@@ -38,8 +60,90 @@ class YuklemeFragment : Fragment() {
     }
 
     fun gorselSec(view : View){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            //read media images
+            if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
+              //izin yok
+                if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_MEDIA_IMAGES)){
+                    //izin mantığını kullanıcıya göstermemiz lazım
+                    Snackbar.make(view,"Galeriye gitmek icin izin vermeniz gerekşyor", Snackbar.LENGTH_INDEFINITE).setAction("İzin ver",
+                        View.OnClickListener{
+                            //izin istememiz lazım
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+                }else{
+                    //isin istememiz lazım
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            }else{
+                //izin var
+                //galeriye gitme kodu
+                val intentToGalery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGalery)
+            }
+
+        }else{
+            //read external strorage
+            if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                //izin yok
+                if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    //izin mantığını kullanıcıya göstermemiz lazım
+                    Snackbar.make(view,"Galeriye gitmek icin izin vermeniz gerekşyor", Snackbar.LENGTH_INDEFINITE).setAction("İzin ver",
+                        View.OnClickListener{
+                            //izin istememiz lazım
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }).show()
+                }else{
+                    //isin istememiz lazım
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }else{
+                //izin var
+                //galeriye gitme kodu
+                val intentToGalery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGalery)
+            }
+        }
+
 
     }
+
+    private fun registerLaunchers(){
+        activityResultLauncher= registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+            if(result.resultCode == RESULT_OK){
+                val intentFromResult = result.data
+                if(intentFromResult != null){
+                    secilenGorsel = intentFromResult.data
+                    try{
+                        if(Build.VERSION.SDK_INT >= 28){
+                            val source = ImageDecoder.createSource(requireActivity().contentResolver,secilenGorsel!!)
+                            secilenBitmap = ImageDecoder.decodeBitmap(source)
+                            binding.imageView.setImageBitmap(secilenBitmap)
+                        }else{
+                            secilenBitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,secilenGorsel)
+                            binding.imageView.setImageBitmap(secilenBitmap)
+                        }
+
+                    }catch (e: Exception){
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){result ->
+            if(result) {
+                //izin verildi
+                val intentToGalery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGalery)
+            }else{
+                //kullanıcı izni reddetti
+                Toast.makeText(requireContext(),"İzni reddettiniz", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
